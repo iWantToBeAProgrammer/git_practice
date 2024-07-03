46ad34264883485a906529b5fe9c2f5e6192c563
rreennn <kerenkaren1704@gmail.com>
2024-07-03 20:02:34 +0700

    dari ren

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("keren pesek");
         System.out.println("jebotak");
+        System.out.println("ciecie");
     }
 }
